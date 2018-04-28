ceb30539e5838c91306fbb40d2be282602495662
knair99 <karunakaran.prasad@gmail.com>
2018-04-28 11:25:14 -0700

    Cookiecutter template for data science projects

---
+++ titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Kaggle titanic data science challenge',
+    author='KK Nair',
+    license='MIT',
+)
